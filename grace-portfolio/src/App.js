246e11585921a8aadb17b3b922d7888f2f1e46e5
Sage Renstrom-Richards <swrenst@umich.edu>
2022-11-29 16:15:09 -0500

    Create app

--- grace-portfolio/src/App.js
+++ grace-portfolio/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Grace and this is my website
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
